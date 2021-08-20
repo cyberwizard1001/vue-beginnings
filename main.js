beac04154d1fc8d6ee565af8a035ded8c52b7e11
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-21 00:53:03 +0530

    Updated

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
     data(){
         return{
-            product: 'Socks'
+            product: 'Socks',
+            description: 'This is a comfortable sock',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.com',
+            inventory: 9,
+            showIfTrue: true
         }
     }
 })
